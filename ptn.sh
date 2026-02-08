43fb60526565d717035afb9409d8fa987ea27ab9
IlMali <malighettiluca08@gmail.com>
2026-02-08 15:51:33 +0100

    aggiunta di amaro

--- ptn.sh
+++ ptn.sh
@@ -2,3 +2,4 @@
 echo "i love PTN"
 echo "la mia canzone preferita è ovviamente Pastello Bianco"
 echo "malighetti per favore non piangere perchè il pastello non è bianco :("
+echo "mi servirà un amaro, per far passar l'amaro in bocca"
